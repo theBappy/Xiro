538482fc38cb75a2c0c7ee58005b27845361b163
theBappy <abappy575@gmail.com>
2025-06-30 11:37:56 +0600

    skeleton of loading when is undefined

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import { EmptyFavorites } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
 import { api } from "@/convex/_generated/api";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -17,18 +18,33 @@ interface BoardListProps {
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
   const data = useQuery(api.boards.get, { orgId });
-  if (data === undefined) return <div>Loading...</div>;
+  if (data === undefined)
+    return (
+      <div>
+        <h2 className="text-3xl">
+          {query.favorites ? "Favorite boards" : "Team Boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    );
 
   if (!data?.length && query.search) return <EmptySearch />;
   if (!data?.length && query.favorites) return <EmptyFavorites />;
   if (!data?.length) return <EmptyBoards />;
 
   return (
-    <div className="">
+    <div>
       <h2 className="text-3xl">
         {query.favorites ? "Favorite boards" : "Team Boards"}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
         {data?.map((board) => (
           <BoardCard
             key={board._id}
