51e3a1ba438abf2d92663464262bccc503f9f9eb
theBappy <abappy575@gmail.com>
2025-07-01 11:51:19 +0600

    layout of the board canvas

---
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -0,0 +1,19 @@
+
+
+export const Toolbar = () => {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
+        <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
+            <div className="">pencil</div>
+            <div className="">square</div>
+            <div className="">circle</div>
+            <div className="">ellipsis</div>
+        </div>
+        <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
+            <div>undo</div>
+            <div>redo</div>
+        </div>
+    </div>
+  )
+}
+
