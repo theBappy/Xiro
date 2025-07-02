52adca0b1cbce3e737de3385d7364b5ead509214
theBappy <abappy575@gmail.com>
2025-07-02 12:00:37 +0600

    canvas state for different types of toolbar property

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,22 +1,40 @@
 'use client'
 
 
+import { useState } from "react"
 import { Info } from "./info"
 import { Participant } from "./participant"
 import { Toolbar } from "./toolbar"
+import { CanvasMode, CanvasState } from "@/types/canvas";
+import { useHistory, useCanUndo, useCanRedo } from "@liveblocks/react"
+
 
 interface CanvasProps {
    boardId: string; 
 }
 
 export const Canvas = ({boardId}:CanvasProps) => {
-   
+   const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+   })
+
+   const history = useHistory()
+   const canUndo = useCanUndo()
+   const canRedo = useCanRedo()
+
     
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info boardId={boardId} />
             <Participant />
-            <Toolbar />
+            <Toolbar  
+            canvasState={canvasState} 
+            setCanvasState={setCanvasState}
+            canRedo={canRedo}
+            canUndo={canUndo}
+            undo={history.undo}
+            redo={history.redo}
+            />
         </main>
     )
 }
